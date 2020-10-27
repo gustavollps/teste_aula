b16c2ef2b77027b3f678a3e73dd5d7d4ba181a11
Ubuntu <ubuntu@ip-172-31-18-61.ec2.internal>
2020-10-27 07:06:03 +0000

    p controller

--- test1.py
+++ test1.py
@@ -1,16 +1,59 @@
 import rospy
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+import tf
+import math
 
+
+kp = 0.01
+
+odom = Odometry()
+scan = LaserScan()
 
 rospy.init_node('cmd_node')
 
+# Auxiliar functions ------------------------------------------------
+def getAngle(msg):
+    quaternion = msg.pose.pose.orientation
+    quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+    euler = tf.transformations.euler_from_quaternion(quat)
+    yaw = euler[2]*180.0/math.pi
+    return yaw
+
+# CALLBACKS ---------------------------------------------------------
+def odomCallBack(msg):
+    global odom
+    odom = msg
+    
+def scanCallBack(msg):
+    global scan
+    scan = msg
+#--------------------------------------------------------------------
+
+# TIMER - Control Loop ----------------------------------------------
 def timerCallBack(event):
+    yaw = getAngle(odom)
+    setpoint = 120
+    error = (setpoint - yaw)
+    
+    if abs(error) > 180:
+        error = -error
+    
+    P = kp*error
+    I = 0
+    D = 0
+    control = P+I+D
+    
     msg = Twist()
-    msg.linear.x = 1.0
+    msg.angular.z = control
     pub.publish(msg)
     
 
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
+scan_sub = rospy.Subscriber('/scan', LaserScan, scanCallBack)
+
 timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
 
 rospy.spin()
