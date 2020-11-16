1155eee2b43422b378d6fec747f309bdfe8135a8
Ubuntu <ubuntu@ip-172-31-18-61.ec2.internal>
2020-11-16 22:36:44 +0000

    -

--- test1.py
+++ test1.py
@@ -7,6 +7,12 @@ import math
 
 
 kp = 1
+ki = 0.5
+Int = 0
+kd = 0.5
+old_error = 0
+
+T = 0.1
 
 odom = Odometry()
 scan = LaserScan()
@@ -33,6 +39,8 @@ def scanCallBack(msg):
 
 # TIMER - Control Loop ----------------------------------------------
 def timerCallBack(event):
+    global kp, ki, kd
+    global Int
     """
     yaw = getAngle(odom)
     setpoint = -45
@@ -51,49 +59,68 @@ def timerCallBack(event):
     error = dist
     """
     
-    setpoint = 0.5
-    
-    scan_len = len(scan.ranges)
-    
-    if scan_len > 0:
-        yaw = getAngle(odom)
+    if estado == 1:
+        setpoint = 0.5
         
-        ind = scan.ranges.index(min(scan.ranges))
-        inc = 2*math.pi / scan_len
-        ang = (ind * inc * 180.0/math.pi) + yaw
-        if ang > 180:
-            ang -= 360
+        scan_len = len(scan.ranges)
+        
+        if scan_len > 0:
+            yaw = getAngle(odom)
             
-        error = (ang - yaw)
-        
-        if abs(error) > 180:
-            if setpoint < 0:
-                error += 360 
-            else:
-                error -= 360
+            ind = scan.ranges.index(min(scan.ranges))
+            inc = 2*math.pi / scan_len
+            ang = (ind * inc * 180.0/math.pi) + yaw
+            if ang > 180:
+                ang -= 360
                 
-        print(ang, yaw, error)
+            error = (ang - yaw)
+            
+            if abs(error) > 180:
+                if setpoint < 0:
+                    error += 360 
+                else:
+                    error -= 360
+                    
+            print(ang, yaw, error)
+            
+            delta_e = error - old_error
+            old_error = error
+            
+            P = kp*error
+            Int += error*T
+            I = Int * ki
+            D = delta_e * kd
+            
+            control = P+I+D
+            if control > 1:
+                control = 1
+            elif control < -1:
+                control = -1
+        else:
+            control = 0        
         
-        P = kp*error
-        I = 0
-        D = 0
-        control = P+I+D
-        if control > 1:
-            control = 1
-        elif control < -1:
-            control = -1
-    else:
-        control = 0        
+        msg = Twist()
+        msg.angular.z = control
+        pub.publish(msg)
+        
+        if error < 1:
+            Int = 0
+            estado = 2
     
-    msg = Twist()
-    msg.angular.z = control
-    pub.publish(msg)
-    
+    elif estado == 2:
+        read = min(scan.ranges)
+        
+        # PID
+        
+        msg = Twist()
+        msg.linear.x = control
+        pub.publish(msg)
+        
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
 scan_sub = rospy.Subscriber('/scan', LaserScan, scanCallBack)
 
-timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
+timer = rospy.Timer(rospy.Duration(T), timerCallBack)
 
 rospy.spin()
